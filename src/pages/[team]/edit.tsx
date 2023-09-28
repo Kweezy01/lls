d086ff68ebe338c6030a8e5ecbe93d4ede4666ce
Kewan Seymour <kewan3272001@gmail.com>
2023-09-28 22:38:53 +0200

    Jargon

--- src/pages/[team]/edit.tsx
+++ src/pages/[team]/edit.tsx
@@ -34,7 +34,7 @@ const Edit = () => {
                   </div>
                   <table className="mt-1 w-full">
                      <tr>
-                        <td className="font-bold text-center border-b border-x bg-blue-500">Team</td><td className="font-bold text-center border-b border-x bg-blue-500">Skipper</td><td className="font-bold text-center border-b border-x bg-blue-500">CO-Angler 2</td><td className="font-bold text-center border-b border-x bg-blue-500">Total Fish</td><td className="font-bold text-center border-b border-x bg-blue-500">Total Weight</td>
+                        <td className="font-bold text-center border-b border-x bg-blue-500">Team</td><td className="font-bold text-center border-b border-x bg-blue-500">Skipper</td><td className="font-bold text-center border-b border-x bg-blue-500">CO-Angler</td><td className="font-bold text-center border-b border-x bg-blue-500">Total Fish</td><td className="font-bold text-center border-b border-x bg-blue-500">Total Weight</td>
                      </tr>
                      <tr className="bg-black bg-opacity-70 text-slate-300 font-bold ml-6 border-b">
                         {[data?.teamName, data?.angler1, data?.angler2, data?.totalFish, data?.totalWeight].map((e) => {
